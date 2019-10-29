a27ac31aee201cd6252fea8d00aebddd57b5d9d5
NawarAli <eng.nawarali@gmail.com>
2019-10-29 03:46:24 +0200

    adding note model that represent the notes data

--- app/src/main/java/com/letsmakethatapp/note/MainActivity.java
+++ app/src/main/java/com/letsmakethatapp/note/MainActivity.java
@@ -3,9 +3,13 @@ package com.letsmakethatapp.note;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.letsmakethatapp.note.models.Note;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
